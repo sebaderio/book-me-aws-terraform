7d25ee7faf45a63806660afabf5b2acd0723dca2
Sebastian Derendarz <derendarz99@gmail.com>
2023-01-24 10:48:20 +0100

    add script pushing API docker image to the registry

---
+++ api/scripts/push_image_to_registry.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+if [[ $# -ne 3 ]]; then
+    echo "Illegal number of parameters."
+    echo "Example usage: ./push_image_to_registry.sh eu-central-1 705130529207.dkr.ecr.eu-central-1.amazonaws.com book-me-prod-api"
+fi
+
+aws ecr get-login-password --region $1 | docker login --username AWS --password-stdin $2
+
+DOCKER_BUILDKIT=1
+docker build -t $3 --target production ../.
+
+docker tag $3 "$2/$3"
+
+# Tag `latest` is added automatically when tag is not specified when building the image.
+docker push "$2/$3:latest"
+
+docker logout $2
