a8afad32caec7113d410e89fb0de42e36f813c05
Sebastian Derendarz <derendarz99@gmail.com>
2023-01-26 14:09:14 +0100

    add healt check to the alb target group

--- terraform/applications/backend/main.tf
+++ terraform/applications/backend/main.tf
@@ -72,6 +72,12 @@ module "alb" {
         cookie_duration = var.alb_api_target_group_stickiness_duration
         type            = "lb_cookie"
       }
+      health_check = {
+        # There are more parameters to be configured, but not specified here because default values
+        # should be fine. For more details check the official Terraform docs for aws_lb_target_group resource.
+        path                = "/auth/ping" # works, but returns 301 when running Django with DEBUG=true, maybe `/auth/ping/?format=json` would be better
+        matcher             = "200-399"
+      }
     }
   ]
 
